9030346db5cee3d5e8ebdaf98dd246d17d22383e
kushalshrestha <kushalshr@gmail.com>
2023-05-15 22:28:32 -0500

    refactor : controller package renamed

--- src/main/java/com/kushal/Spring/Security/democontroller/DemoController.java
+++ src/main/java/com/kushal/Spring/Security/democontroller/DemoController.java
@@ -1,4 +1,4 @@
-package com.kushal.Spring.Security.DemoController;
+package com.kushal.Spring.Security.democontroller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
